dbe3ed89a4c6234e329224695f12c67c3f44e106
GledsonS831 <glegogle84@gmail.com>
2020-08-17 23:52:21 -0300

    :hammer: add updateliveusers

---
+++ node/event/liveUsersUpdate.ts
@@ -0,0 +1,9 @@
+import { EventContext } from "@vtex/api";
+
+import { Clients } from "..";
+export async function updateLiveUsers(ctx: EventContext<Clients>) {
+  const liveUsersProducts = await ctx.clients.analytics.getLiveUsers();
+  console.log("LIVE USERS: ", liveUsersProducts);
+  
+  return true;
+}
